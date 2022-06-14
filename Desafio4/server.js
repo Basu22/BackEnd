a9bc4bcc8bdad1f0362e6946d1be91a8d5f00efa
basilio ossvald <basilio.ossvald@gmail.com>
2022-06-14 11:10:11 -0300

    Desafio4

--- Desafio4/server.js
+++ Desafio4/server.js
@@ -1,29 +1,9 @@
-// primero - set de conexión
 const express = require('express')
 const app = express()
 const puerto = 8080
-// segundo - ruteo
-const rutasMascotas = require('./routes/rutasMascotas')
-const rutasPersonas = require('./routes/rutasPersonas')
-//configuración para trabajar el body como json
-app.use(express.json())
-app.use(express.urlencoded({ extended:true }))
-app.use('/imagenes',express.static('public'))
-app.use(express.static('html'))
 
-//ruteamos a mascotas y personas
-app.use('/mascotas',rutasMascotas)
-app.use('/personas',rutasPersonas)
 
-//podemos enviar por get la ruta hacia un archivo estatico 
-//en este caso estamos mostrando el index.html en la raiz del localhost
-app.get('/',(req,res)=>{
-    res.sendFile( __dirname + '/index.html')
+app.listen(puerto, (req,res)=>{
+    console.log(`Escuchando el puerto ${puerto}`)
 })
-
-//creamos la conexion al 8080
-app.listen(8080,()=>{
-    console.log(`Escuchando el puerto: ${puerto}` )
-})
-
 
